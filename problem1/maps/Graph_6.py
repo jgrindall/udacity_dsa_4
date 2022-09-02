e33c6276980610bd7033ea16121af802b13a3a2c
jgrindall <john.grindall@gmail.com>
2022-09-02 10:05:40 +0100

    use euclidean

--- problem1/maps/Graph_6.py
+++ problem1/maps/Graph_6.py
@@ -62,36 +62,5 @@ class Graph_6(AGraph):
                 4
             ]
         ]
-        
-        road_lengths = {
-           0:{
-              1:2,
-              2:2,
-              4:6,
-              5:10
-           },
-           1:{
-              0:2,
-              3:2
-           },
-           2:{
-             0:2,
-             3:4
-           },
-           3:{
-              1:2,
-              2:4,
-              5:2
-           },
-           4:{
-              0:6,
-              5:2
-           },
-           5:{
-               0:10,
-               3:2,
-               4:2
-             }
-        }
-                
-        super().__init__(cities, roads, road_lengths)
+
+        super().__init__(cities, roads)
